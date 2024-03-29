638737999049096334120139a09169e27377d639
manchenkoff <artem@manchenkoff.me>
2024-03-29 23:55:13 +0100

    feat: add Origin header to the request

--- src/runtime/composables/useSanctumUser.ts
+++ src/runtime/composables/useSanctumUser.ts
@@ -1,13 +1,13 @@
-import { useState, useRuntimeConfig } from '#app';
+import { useState } from '#app';
 import { type Ref } from 'vue';
-import type { SanctumModuleOptions } from '../../types';
+import { useSanctumConfig } from './useSanctumConfig';
 
 /**
  * Returns a current authenticated user information.
  * @returns Reference to the user state as T.
  */
 export const useSanctumUser = <T>(): Ref<T | null> => {
-    const options = useRuntimeConfig().public.sanctum as SanctumModuleOptions;
+    const options = useSanctumConfig();
 
     const user = useState<T | null>(options.userStateKey, () => null);
 
