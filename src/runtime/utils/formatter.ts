f9b52003416a3cc1442f32ac00022b598082dfcd
Oliver Scase <oliver@techlove.se>
2024-09-03 18:44:14 +0200

    Fix: change method of removing trailing slash from url to support ios <17

--- src/runtime/utils/formatter.ts
+++ src/runtime/utils/formatter.ts
@@ -1,3 +1,6 @@
 export function trimTrailingSlash(path: string): string {
-  return path.replace(/(?<!^)\/$/, '')
+  if (path.length > 1 && path.slice(-1) === '/') {
+    return path.slice(0, -1);
+  }
+  return path;
 }
