104b5926e71c9308556a46ec42684e565a468408
Oliver Scase <oliver@techlove.se>
2024-09-04 00:23:26 +0200

    Chore: fixed linting

--- src/runtime/utils/formatter.ts
+++ src/runtime/utils/formatter.ts
@@ -1,6 +1,6 @@
 export function trimTrailingSlash(path: string): string {
   if (path.length > 1 && path.slice(-1) === '/') {
-    return path.slice(0, -1);
+    return path.slice(0, -1)
   }
-  return path;
+  return path
 }
