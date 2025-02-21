bbb9e049561816963f58d12a7aae2fd6766f9258
manchenkoff <artem@manchenkoff.me>
2025-02-21 22:20:22 +0100

    fix: added type hints for fetch composables

--- src/runtime/composables/useLazySanctumFetch.ts
+++ src/runtime/composables/useLazySanctumFetch.ts
@@ -1,14 +1,18 @@
 import type { UseFetchOptions } from 'nuxt/app'
+import type { FetchError } from 'ofetch'
 import { useLazyFetch, useSanctumClient } from '#imports'
+import type { KeysOf, PickFrom, AsyncData } from '#app/composables/asyncData'
+
+type LazyFetchResponse<T> = AsyncData<PickFrom<T, KeysOf<T>> | null, FetchError | null | undefined>
 
 export function useLazySanctumFetch<T>(
   url: string | (() => string),
   options?: UseFetchOptions<T>,
-) {
+): LazyFetchResponse<T> {
   const client = useSanctumClient()
 
   return useLazyFetch(url, {
     ...options,
     $fetch: client as typeof $fetch,
-  })
+  }) as LazyFetchResponse<T>
 }
--- src/runtime/composables/useSanctumFetch.ts
+++ src/runtime/composables/useSanctumFetch.ts
@@ -1,14 +1,18 @@
 import type { UseFetchOptions } from 'nuxt/app'
+import type { FetchError } from 'ofetch'
 import { useFetch, useSanctumClient } from '#imports'
+import type { AsyncData, KeysOf, PickFrom } from '#app/composables/asyncData'
+
+type FetchResponse<T> = AsyncData<PickFrom<T, KeysOf<T>> | null, FetchError | null | undefined>
 
 export function useSanctumFetch<T>(
   url: string | (() => string),
   options?: UseFetchOptions<T>,
-) {
+): FetchResponse<T> {
   const client = useSanctumClient()
 
   return useFetch(url, {
     ...options,
     $fetch: client as typeof $fetch,
-  })
+  }) as FetchResponse<T>
 }
