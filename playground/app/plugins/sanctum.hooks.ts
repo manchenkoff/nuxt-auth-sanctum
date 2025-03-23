021f862e3096e11f92d6083be585e92dd63895a5
manchenkoff <artem@manchenkoff.me>
2025-03-23 01:14:58 +0100

    playground: added sanctum:redirect hook example

--- playground/app/plugins/sanctum.hooks.ts
+++ playground/app/plugins/sanctum.hooks.ts
@@ -2,4 +2,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('sanctum:error', (response) => {
     console.log('Sanctum error hook triggered', response)
   })
+
+  nuxtApp.hook('sanctum:redirect', (url) => {
+    console.log('Sanctum redirect hook triggered', url)
+  })
 })
