8af78788520b8b82690dee0d21299cddd1ddd8a8
roiLeo <medina.leo42@gmail.com>
2025-09-03 09:02:51 +0200

    ✨ Nuxt UI v4

--- docs/app/pages/[...slug].vue
+++ docs/app/pages/[...slug].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { ContentNavigationItem } from '@nuxt/content'
-import { findPageHeadline } from '#ui-pro/utils/content'
+import { findPageHeadline } from '@nuxt/content/utils'
 
 definePageMeta({
   layout: 'docs'
