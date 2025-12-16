8074e1463eb073fd0bb45d16dbe36fa30a777efa
manchenkoff <artem@manchenkoff.me>
2025-12-16 19:38:21 +0000

    chore: minor refactoring for proxy endpoint

--- src/runtime/server/api/proxy.ts
+++ src/runtime/server/api/proxy.ts
@@ -1,9 +1,6 @@
-import {
-
-  appendResponseHeader, defineEventHandler, getRequestHeaders, getQuery, setResponseStatus,
-} from 'h3'
+import { appendResponseHeader, defineEventHandler, getRequestHeaders, getQuery, setResponseStatus } from 'h3'
 import type { H3Event, EventHandlerRequest, HTTPMethod } from 'h3'
-import { $fetch } from 'ofetch'
+import { $fetch, type FetchResponse } from 'ofetch'
 import { useSanctumLogger } from '../../utils/logging'
 import { determineCredentialsMode } from '../../utils/credentials'
 import type { ModuleOptions } from '../../types/options'
@@ -16,29 +13,48 @@ export default defineEventHandler(async (event: H3Event<EventHandlerRequest>) =>
   const config = useRuntimeConfig().sanctum as ModuleOptions
   const logger = useSanctumLogger(config.logLevel)
 
+  const endpoint = assembleEndpoint(event, config.serverProxy.baseUrl)
+
+  logger.debug(`[sanctum] proxying request to ${endpoint}`)
+
+  const response = await proxyRequest(event, endpoint)
+
+  prepareResponse(event, response)
+
+  logger.debug(`[sanctum] proxying response from ${endpoint}`)
+
+  return response._data
+})
+
+function assembleEndpoint(event: H3Event<EventHandlerRequest>, baseUrl: string): string {
+  return `${baseUrl}/${event.context.params?._}`
+}
+
+function buildHeaders(event: H3Event<EventHandlerRequest>): HeadersInit {
+  return {
+    accept: 'application/json',
+    ...getRequestHeaders(event),
+  }
+}
+
+async function proxyRequest(event: H3Event<EventHandlerRequest>, endpoint: string): Promise<FetchResponse<unknown>> {
   const
     method = event.method,
     query = getQuery(event),
     body = METHODS_WITH_BODY.includes(method) ? event.node.req : undefined,
-    headers = getRequestHeaders(event),
-    proxyRoute = event.context.params?._
+    headers = buildHeaders(event)
 
-  const targetUrl = `${config.serverProxy.baseUrl}/${proxyRoute}`
-
-  logger.debug(`[sanctum] proxying request to ${targetUrl}`)
-
-  const response = await $fetch.raw(targetUrl, {
+  return await $fetch.raw(endpoint, {
     method: method,
     query: query,
     body: body,
     credentials: determineCredentialsMode(),
-    headers: {
-      accept: 'application/json',
-      ...headers,
-    } as HeadersInit,
+    headers: headers,
     ignoreResponseError: true,
   })
+}
 
+function prepareResponse(event: H3Event<EventHandlerRequest>, response: FetchResponse<unknown>): void {
   response.headers.forEach((value, key) => {
     if (HEADERS_TO_IGNORE.includes(key.toLowerCase())) {
       return
@@ -48,8 +64,4 @@ export default defineEventHandler(async (event: H3Event<EventHandlerRequest>) =>
   })
 
   setResponseStatus(event, response.status)
-
-  logger.debug(`[sanctum] proxying response from ${targetUrl}`)
-
-  return response._data
-})
+}
