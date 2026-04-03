127f9b0bc282dbb7993fe4e2877934dde61ecb30
Artem Manchenkov <artem@manchenkoff.me>
2026-04-03 12:01:28 +0100

    fix: deduplicate fetch requests using key (#581)

--- src/runtime/composables/useLazySanctumFetch.ts
+++ src/runtime/composables/useLazySanctumFetch.ts
@@ -1,5 +1,5 @@
 import { type UseFetchOptions, useLazyFetch } from '#app'
-import type { MaybeRefOrGetter } from 'vue'
+import { toRaw, toValue, type MaybeRefOrGetter } from 'vue'
 import { useSanctumClient } from '../composables/useSanctumClient'
 import type { SanctumFetchResponse } from '../types/fetch'
 
@@ -8,7 +8,9 @@ export function useLazySanctumFetch<T>(
   options?: Omit<UseFetchOptions<T>, 'lazy'>,
 ): SanctumFetchResponse<T> {
   const client = useSanctumClient() as typeof $fetch
-  const params = { ...options, $fetch: client } as UseFetchOptions<T>
+  const key = options?.key ?? JSON.stringify([toRaw(toValue(url)), toRaw(toValue(options))])
+
+  const params = { ...options, key, $fetch: client } as UseFetchOptions<T>
 
   // @ts-expect-error unable to satisfy params<T>
   return useLazyFetch<T>(url, params)
